f72a87f4ee3195b0ec9c49c0dc56bab74d76170d
Owner <ekrmaawad231@gmail.com>
2019-09-23 09:42:30 +0300

    ttyftftyyyyg

--- app/src/main/java/com/aboklaib/moaadtaskmanger/signin.java
+++ app/src/main/java/com/aboklaib/moaadtaskmanger/signin.java
@@ -2,8 +2,11 @@ package com.aboklaib.moaadtaskmanger;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class signin extends AppCompatActivity {
     private EditText emsign,pssign;
@@ -21,6 +24,52 @@ public class signin extends AppCompatActivity {
 
 
 
-    }
+        signIn1.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dataHandler();
+            }
 
-}
+            private void dataHandler() {
+                String email=emsign.getText().toString();
+                String password=pssign.getText().toString();
+                boolean isok=true;
+                if (email.length()<4)
+                {
+                    if (email.indexOf( "@")<0|| email.indexOf( ".")<0{
+                    {
+                        emsign.setError( ("email vrong format") );
+                        isok=false;
+                    }
+                    if (isok) {
+                        if (password.length() < 8) {
+                            pssign.setError( "password length eror" );
+                        }
+                        public boolean isValidEmailAddress (String email)
+                        {
+
+                        }
+                        private void signIn (String, email, String password)
+                        {
+                            FirebaseAuth auth = FirebaseAuth.getInstance();
+                            auth.signInWithEmailAndPassword( email,password. );
+                        }
+                    }
+
+
+
+
+                    //signIn(email,passw)
+
+
+
+                }
+                }};
+                }
+            String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
+            java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
+            java.util.regex.Matcher m = p.matcher(email);
+            return m.matches();
+
+        }
+
