c28c4fe07ce8a88bb0e5bc296b433cbb561f95a5
Owner <ekrmaawad231@gmail.com>
2019-09-24 12:24:12 +0300

    ttyftftyyyyg

---
+++ app/src/main/java/com/aboklaib/moaadtaskmanger/TempAllTaskActivity.java
@@ -0,0 +1,14 @@
+
+package com.aboklaib.moaadtaskmanger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TempAllTaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_temp_all_task );
+    }
+}
