e227ddfcf3cec1339ab629e202fe5b434942032f
Owner <ekrmaawad231@gmail.com>
2019-09-24 12:24:00 +0300

    ttyftftyyyyg

--- app/src/main/java/com/aboklaib/moaadtaskmanger/signin.java
+++ app/src/main/java/com/aboklaib/moaadtaskmanger/signin.java
@@ -1,11 +1,16 @@
 package com.aboklaib.moaadtaskmanger;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class signin extends AppCompatActivity {
@@ -17,10 +22,10 @@ public class signin extends AppCompatActivity {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_signin );
 
-        emsign=findViewById( R.id.emSign);
-        pssign=findViewById( R.id.psSign );
-        signIn1=findViewById( R.id.signIn1);
-        signUp1=findViewById( R.id.signUp1 );
+        emsign = findViewById( R.id.emSign );
+        pssign = findViewById( R.id.psSign );
+        signIn1 = findViewById( R.id.signIn1 );
+        signUp1 = findViewById( R.id.signUp1 );
 
 
 
@@ -30,46 +35,59 @@ public class signin extends AppCompatActivity {
                 dataHandler();
             }
 
-            private void dataHandler() {
-                String email=emsign.getText().toString();
-                String password=pssign.getText().toString();
-                boolean isok=true;
-                if (email.length()<4)
-                {
-                    if (email.indexOf( "@")<0|| email.indexOf( ".")<0{
-                    {
-                        emsign.setError( ("email vrong format") );
-                        isok=false;
-                    }
-                    if (isok) {
-                        if (password.length() < 8) {
-                            pssign.setError( "password length eror" );
-                        }
-                        public boolean isValidEmailAddress (String email)
-                        {
 
-                        }
-                        private void signIn (String, email, String password)
-                        {
-                            FirebaseAuth auth = FirebaseAuth.getInstance();
-                            auth.signInWithEmailAndPassword( email,password. );
-                        }
-                    }
+                });
+    }
+    private void dataHandler() {
+        String email=emsign.getText().toString();
+        String password=pssign.getText().toString();
+        boolean isok=true;
 
+            if(isValidEmailAddress( email )) {
+                emsign.setError( "email" );
+                isok=false;
+            }
+            if (password.length() < 8) {
+                pssign.setError( "password length eror" );
+                isok=false;
+            }
+            if(isok)
+            {
 
-
-
-                    //signIn(email,passw)
-
-
-
-                }
-                }};
-                }
-            String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
-            java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
-            java.util.regex.Matcher m = p.matcher(email);
-            return m.matches();
+            }
 
         }
+    private void signIn(String email,String pass)
+    {
+        FirebaseAuth auth=FirebaseAuth.getInstance();
+        auth.signInWithEmailAndPassword(email,pass).addOnCompleteListener( this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful())
+                {
+                    //todo go to ,main screen(all task activity)
+                    Intent i=new Intent( getApplicationContext(),TempAllTaskActivity.class );
+                    startActivity( i );
+                }
+                else
+                {
+                    emsign.setError( "email or password is wrong" );
+                }
+                }
+            }
+       );
+    }
+
+
+
+    public boolean isValidEmailAddress (String email)
+    {
+        String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
+        java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
+        java.util.regex.Matcher m = p.matcher(email);
+        return m.matches();
+
+    }
+}
+
 
