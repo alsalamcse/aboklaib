c4d0d20e0695501d4cd4e0fa6889d467f136b71a
Owner <ekrmaawad231@gmail.com>
2019-09-29 12:23:06 +0300

    ttyftftyyyyg

--- app/src/main/java/com/aboklaib/moaadtaskmanger/signup.java
+++ app/src/main/java/com/aboklaib/moaadtaskmanger/signup.java
@@ -1,9 +1,15 @@
 package com.aboklaib.moaadtaskmanger;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class signup extends AppCompatActivity
 {
@@ -25,23 +31,55 @@ public class signup extends AppCompatActivity
         save1=findViewById( R.id.save1 );
 
     }
-    boolean isok=true;//if all the fields filled well
-    String email=em2.getText().toString();
-    String passw2=pass2.getText().toString();
-    String fname1=fname.getText().toString();
-    String lname1=Lname.getText().toString();
-    String rewarte1=rewarte.getText().toString();
-    String btnsave1=save1.getText().toString();
-    if(email.length()<4 || email.indexOf('@')<0 || email.indexOF('.')<0)
+
+    public void dattaHandler()
     {
-        em2.setError( "Wrong Email" );
-        isok = false;
+        boolean isok=true;//if all the fields filled well
+        String email=em2.getText().toString();
+        String passw1=pass2.getText().toString();
+        String passw2=rewarte.getText().toString();
+        String fname1=fname.getText().toString();
+        String lname1=Lname.getText().toString();
+        String btnsave1=save1.getText().toString();
+        if(email.length()<4 || email.indexOf('@')<0 || email.indexOf('.')<0)
+        {
+            em2.setError( "Wrong Email" );
+            isok = false;
+        }
+        if (passw2.length()<8 ||  passw1.equals( passw2 )==false{
+            pass2.setError( "have to at least 8 char and the same password" );
+            rewarte.setError( "have to be at least 8 char and the same password" );
+            isok=false;
+    }
+    if (fname1.length()==0)
+    {
+        fname.setError( "enter name" );
+        isok=false;
+
+    }
+    if (isok)
+    {
+        createAcount(email,passw1,fname1,lname1,phone);
+
     }
 
-    public String getPassw2() {
-
-    }if ()
-
     }
 
+    private void createAcount(String email, String passw1, String fname1, String lname1, EditText phone) {
+     FirebaseAuth auth = FirebaseAuth.getInstance();
+     auth.createUserWithEmailAndPassword( email,passw1 ).addOnCompleteListener( this, new OnCompleteListener<AuthResult>() {
+         @Override
+         public void onComplete(@NonNull Task<AuthResult> task) {
+             if (task.isSuccessful())
+             {
+                 finish();
+             }
+             else {
+                 em2.setError( "sign up failed" );
+             }
+
+             }
+     } );
+    }
+}
 
